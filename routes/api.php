2315daf2acfd7986157d7c05c04b956714fbcfd4
Petrenko Hryhorii <hryhorii.petrenko@student.howest.be>
2025-08-11 16:59:07 +0200

    Implemented the needed routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestaurantController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/restaurants', [RestaurantController::class, 'getAllRestaurants']);
+Route::get('/restaurants/{restaurant}/bookings', [RestaurantController::class, 'getBookings']);
+Route::post('/bookings', [RestaurantController::class, 'addBooking']);
