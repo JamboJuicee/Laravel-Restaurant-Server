6e4785e70ba2cdcfae579c34e9f4ab0d2881617c
Petrenko Hryhorii <hryhorii.petrenko@student.howest.be>
2025-08-11 12:22:53 +0200

    implemented correct migration structure

--- database/migrations/2025_08_11_101435_create_restaurants_table.php
+++ database/migrations/2025_08_11_101435_create_restaurants_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('restaurants', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('name', 50);
+            $table->integer('rating');
             $table->timestamps();
         });
     }
