84da67c7bcb5506c65bb26ebcdb54a1ad7ac6145
Petrenko Hryhorii <hryhorii.petrenko@student.howest.be>
2025-08-11 12:50:24 +0200

    fixed HasUuids import

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\HasUuids;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Booking extends Model
 {
